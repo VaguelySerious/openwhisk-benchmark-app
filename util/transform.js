bdf9cbb8b32de4b109e6cbcd73ac006e5d0f5037
VaguelySerious <mittgfu@gmail.com>
2020-03-28 13:34:41 +0100

    add very simple transformation function to color by height

--- util/transform.js
+++ util/transform.js
@@ -1,31 +1,51 @@
 function invertColors() {
   for (var y = 0; y < this.height; y++) {
     for (var x = 0; x < this.width; x++) {
-      var idx = (this.width * y + x) * 4;
+      var idx = (this.width * y + x) * 4
 
-      this.data[idx] = 255 - this.data[idx];
-      this.data[idx + 1] = 255 - this.data[idx + 1];
-      this.data[idx + 2] = 255 - this.data[idx + 2];
+      this.data[idx] = 255 - this.data[idx]
+      this.data[idx + 1] = 255 - this.data[idx + 1]
+      this.data[idx + 2] = 255 - this.data[idx + 2]
     }
   }
 }
 
 function colorByHeight() {
+  let max = 0
+  let min = 99999999999
   for (var y = 0; y < this.height; y++) {
     for (var x = 0; x < this.width; x++) {
-      var idx = (this.width * y + x) * 4;
+      var idx = (this.width * y + x) * 4
+      const height = this.data[idx]
 
-      //   this.data[idx] = 255 - this.data[idx];
-      //   this.data[idx + 1] = 255 - this.data[idx + 1];
-      //   this.data[idx + 2] = 255 - this.data[idx + 2];
-      //   if (x === 255 && y === 64) {
-      //     return;
-      //   }
+      if (height < min) {
+        min = height
+      }
+      if (height > max) {
+        max = height
+      }
+
+      if (height >= 11 && height < 30) {
+        this.data[idx] = 0
+        this.data[idx + 1] = 60
+        this.data[idx + 2] = 255
+      }
+      if (height >= 30 && height < 60) {
+        this.data[idx] = 0
+        this.data[idx + 1] = 255
+        this.data[idx + 2] = 0
+      }
+      if (height >= 60 && height < 156) {
+        this.data[idx] = 255
+        this.data[idx + 1] = 255
+        this.data[idx + 2] = 255
+      }
     }
   }
+  console.log(max, min)
 }
 
 module.exports = {
   colorByHeight,
-  invertColors
-};
+  invertColors,
+}
